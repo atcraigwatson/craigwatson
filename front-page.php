16e2c67745d3502132888ba59ca8066e10fad485
Craig Waton <craig@watsonmemorials.co.uk>
2024-11-19 19:45:17 +0000

    Make the cols more responsive across all devices

--- front-page.php
+++ front-page.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
   <section class="section-home-jumbotron d-flex align-items-center">
     <div class="py-5 container">
       <div class="row py-lg-5">
-        <div class="col-lg-6 mx-auto">
+        <div class="col-md-9 col-lg-8 col-xl-7 mx-auto">
           <h1 class="text-uppercase display-2 fw-bold"><span class="home-h1-pre-text">Hi, my name is</span><br>Craig Watson</h1>
           <p class="lead fs-2 fw-bold text-body-tertiary">An aspiring front-end developer with a passion for WordPress.</p>
           <a href="/about-me" class="btn btn-secondary me-3">More About Me</a>
