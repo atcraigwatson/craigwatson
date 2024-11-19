631280618ff41ce28785fe4e0ca6355613c9cd9e
Craig Waton <craig@watsonmemorials.co.uk>
2024-11-19 16:25:00 +0000

    Add google fonts to header.php
    Add the google recommended code for the fonts to header.php

--- header.php
+++ header.php
@@ -24,6 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     <link rel="icon" type="image/png" href="/assets/images/cockertoncc-logo-icon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?> >
