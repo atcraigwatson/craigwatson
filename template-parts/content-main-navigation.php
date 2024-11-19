b25007488e888e28b2016cafd5d6c6a07f602345
Craig Waton <craig@watsonmemorials.co.uk>
2024-11-19 19:44:30 +0000

    Add navbar collapse

--- template-parts/content-main-navigation.php
+++ template-parts/content-main-navigation.php
@@ -1,20 +1,25 @@
     <header>
-      <nav class="navbar" data-bs-theme="dark">
+      <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">
             craigwatson.io
           </a>
-          <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://github.com/atcraigwatson" target="blank">Github</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://codepen.io/atcraigwatson" target="blank">Codepen</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/contact-me">Contact Me</a>
-            </li>
-          </ul>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="https://github.com/atcraigwatson" target="blank">Github</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://codepen.io/atcraigwatson" target="blank">Codepen</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact-me">Contact Me</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </header>
