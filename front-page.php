09923a074fcfd00229a6210df075d66da0ffd016
Craig Waton <craig@watsonmemorials.co.uk>
2024-11-20 10:07:23 +0000

    Rearrange h1 text
    The br element that followed the span was creating extra space that was not easily removed with CSS. The span is now above the h1 element.

--- front-page.php
+++ front-page.php
@@ -24,7 +24,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     <div class="py-5 container">
       <div class="row py-lg-5">
         <div class="col-md-9 col-lg-8 col-xl-7 mx-auto">
-          <h1 class="text-uppercase display-2 fw-bold"><span class="home-h1-pre-text">Hi, my name is</span><br>Craig Watson</h1>
+          <span class="home-h1-pre-text">Hi, my name is</span>
+          <h1 class="text-uppercase display-2 fw-bold">Craig Watson</h1>
           <p class="lead fs-2 fw-bold text-body-tertiary">An aspiring front-end developer with a passion for WordPress.</p>
           <a href="/about-me" class="btn btn-secondary me-3">More About Me</a>
           <a href="/projects" class="btn btn-outline-secondary">Recent Projects</a>
