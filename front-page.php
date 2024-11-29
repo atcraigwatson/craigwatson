c0c474b3511f2aca83d3313ad060a6860005a567
Craig Waton <craig@watsonmemorials.co.uk>
2024-11-29 14:58:06 +0000

    Updates classes

--- front-page.php
+++ front-page.php
@@ -24,11 +24,11 @@ if ( ! defined( 'ABSPATH' ) ) {
     <div class="py-5 container">
       <div class="row py-lg-5">
         <div class="col-md-9 col-lg-8 col-xl-7 mx-auto">
-          <span class="home-h1-pre-text">Hi, my name is</span>
-          <h1 class="text-uppercase display-2 fw-bold">Craig Watson</h1>
-          <p class="lead fs-2 fw-bold text-body-tertiary">An aspiring front-end developer with a passion for WordPress.</p>
-          <a href="/about-me" class="btn btn-secondary me-3">More About Me</a>
-          <a href="/projects" class="btn btn-outline-secondary">Recent Projects</a>
+          <span class="font-monospace text-lowercase text-primary fs-4">Hi, my name is</span>
+          <h1 class="text-uppercase display-2 fw-bold mb-0">Craig Watson</h1>
+          <p class="fs-2 fw-bold text-body-tertiary mb-4">An aspiring front-end developer with a passion for WordPress.</p>
+          <a href="/about-me" class="btn btn-primary me-3">More About Me</a>
+          <a href="/projects" class="btn btn-outline-primary">Recent Projects</a>
         </div>
       </div>
     </div>
